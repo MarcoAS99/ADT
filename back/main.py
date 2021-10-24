cce4fdd940d5a01528c6f3dea394135e2361e095
MarcoAS99 <antonioo.dsm@gmail.com>
2021-10-24 13:24:18 +0200

    Registro de usuario conexion inicial entre back y front

--- back/main.py
+++ back/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from models import User
 
 app = FastAPI()
 
@@ -13,3 +14,13 @@ templates = Jinja2Templates(directory="../html")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post('/', status_code=status.HTTP_201_CREATED)
+async def register(request: Request):
+    form = await request.form()
+    user_reg = User()
+    if await user_reg.register(form.get("name"), form.get("email"), form.get("phone"), form.get("password"), ''):
+        return templates.TemplateResponse("login.html", {"request": request})
+    else:
+        return {"error": user_reg.error_list}
--- back/models.py
+++ back/models.py
@@ -1,8 +1,29 @@
 from pydantic import BaseModel
+import re
 
 
 class User(BaseModel):
-    name: str
-    email: str
-    phone: int
-    paymentMode: str
+    error_list = []
+
+    async def is_valid(self, name: str, email: str, phone: int, password: str):
+        print('Inicio is valid')
+        if len(name) > 50:
+            self.error_list.append('Username max chars is 50.')
+        if not (email.__contains__("@")):
+            self.error_list.append('Email format wrong.')
+        if len(phone) != 9:
+            self.error_list.append('Length of phone number incorrect.')
+        if not re.match(r"^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,16}$", password):
+            self.error_list.append(
+                'Password must contain at least one digit, one uppercase letter, one lowercase letter, one special character and have a length between 6 and 16.')
+        if not self.error_list:
+            return True
+        return False
+
+    async def register(self, name: str, email: str, phone: int, password: str, paymentMode: str):
+        if await self.is_valid(name, email, phone, password):
+            # TODO: METER DATOS DE USUARIO EN BBDD
+            print('OK')
+            return True
+        else:
+            return False
